b75f9756dea22138d5549d5c6db9992482c0f24a
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2021-11-05 16:07:03 +0300

    fix comment

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -8,7 +8,7 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    """It is the main function."""
+    """Define the main function."""
     print('Welcome to the Brain Games!')
     welcome_user()
 
