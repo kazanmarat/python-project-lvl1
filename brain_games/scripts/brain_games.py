856d554798118e4d22c8f98ae64a4a592d03f532
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2021-11-05 13:14:36 +0300

    fix some lines in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
+
+"""Main module."""
+
+
 from brain_games.cli import welcome_user
 
+
 def main():
+    """It is the main function."""
     print('Welcome to the Brain Games!')
-   
     welcome_user()
-    
+
 
 if __name__ == '__main__':
     main()
-
