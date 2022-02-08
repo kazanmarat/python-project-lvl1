6fa116c27d3a826d7a5823a7f37e4a52c2708db8
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-02-08 20:18:49 +0300

    add brain_games/scripts/brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Do this module main and import functions from other modules."""
+
+from games.do_gcd import do_gcd
+
+
+def main():
+    """Define the main function."""
+    do_gcd()
+
+
+if __name__ == '__main__':
+    main()
