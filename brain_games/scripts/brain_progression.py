b685b17a9c9bcd6a07f812675ae9dbdeb5ae689d
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-02-09 20:13:57 +0300

    add brain_games/scripts/brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Do this module main and import functions from other modules."""
+
+from games.do_progr import do_progr
+
+
+def main():
+    """Define the main function."""
+    do_progr()
+
+
+if __name__ == '__main__':
+    main()
