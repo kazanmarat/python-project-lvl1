29a25d0af7d2c2ce251b195ff5832a69cd75e336
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-02-10 13:27:33 +0300

    fix give_rand_numb.py

--- games/give_rand_numb.py
+++ games/give_rand_numb.py
@@ -4,5 +4,5 @@ from random import randint
 
 
 def give_rand_int():
-    """Found and returns random integer from 1 to 30."""
+    """Found and returns random integer from 1 to 100."""
     return randint(1, 100)
