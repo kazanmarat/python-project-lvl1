6336666ea5da3f83ed54696c3bb1b73af6545b07
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-02-10 13:29:44 +0300

    add brain_games/scripts/brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Do this module main and import functions from other modules."""
+
+from games.do_prime import do_prime
+
+
+def main():
+    """Define the main function."""
+    do_prime()
+
+
+if __name__ == '__main__':
+    main()
