ee5c693e9371b90b953a40b54966a09bd936685a
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-02-11 17:19:55 +0300

    fix brain_games/even_numbers.py

--- brain_games/even_numbers.py
+++ brain_games/even_numbers.py
@@ -6,6 +6,10 @@ import prompt
 
 print('Welcome to the Brain Games!')
 
+text_promt = 'Answer "yes" if the number is even, otherwise answer "no. '
+text_wrong_ans1 = "'yes' is wrong answer ;(. Correct answer was 'no'."
+text_wrong_ans2 = "Let\'s try again, {0}."
+
 
 def even_numb():
     """Greet and ask a name."""
@@ -16,9 +20,7 @@ def even_numb():
     while count < 3:
         num = randint(0, 100)
         print(num)
-        answ = prompt.string('Answer "yes" if the '
-                               'number is even, '
-                               'ohterwise answer "no". ')
+        answ = prompt.string(text_promt)
         n_even = num % 2
         if (n_even == 0 and answ == 'yes') or (n_even != 0 and answ == 'no'):
             print('Your answer: {0}\nCorrect!'.format(answ))
@@ -26,12 +28,10 @@ def even_numb():
             if count == 3:
                 print('Congratulations, {0}!'.format(name))
         elif (n_even % 2 != 0 and answ != 'no'):
-            print(
-                "'yes' is wrong answer ;(. Correct "
-                "answer was 'no'.\nLet\'s try again, {0}.".format(name))
+            print(text_wrong_ans1)
+            print(text_wrong_ans2.format(name))
             break
         elif (n_even % 2 == 0 and answ != 'yes'):
-            print(
-                "'no' is wrong answer ;(. Correct "
-                "answer was 'yes'.\nLet\'s try again, {0}.".format(name))
+            print(text_wrong_ans1)
+            print(text_wrong_ans2.format(name))
             break
