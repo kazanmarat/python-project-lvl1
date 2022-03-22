3c79a492e6e21300940ea6ed5970f72d00f3921a
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-03-22 15:13:51 +0300

    fix some files for linter

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -5,6 +5,7 @@
 from brain_games.games.do_gcd import do_gcd
 from brain_games.central_function import central_function
 
+
 def main():
     """Define the main function."""
     central_function(do_gcd)
