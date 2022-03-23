befb30c350d89fdcb831370c36239ea9e32a61cd
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2022-03-23 20:29:49 +0300

    process of refactoring

--- brain_games/games/do_even.py
+++ brain_games/games/do_even.py
@@ -1,6 +1,8 @@
 """Module do_even. Calculates evenness of given number."""
 
 from random import randint
+from brain_games.dispatch_all import dispatch_all
+
 
 LOWER_BOUND = 1
 UPPER_BOUND = 100
@@ -11,10 +13,17 @@ def do_even():
     Parameter: operat_question and correct result.
     Returns:  is random_number is even.
     """
+    
+    FUNC_TXT = 'Answer "yes" if the number is even, otherwise answer "no".'
     random_number = randint(LOWER_BOUND, UPPER_BOUND)
     if random_number % 2 == 0:
         correct_result = 'yes'
     else:
         correct_result = 'no'
-    operat_question = 'Question: {0}'.format(random_number)
-    return (operat_question, correct_result)
+    operat_question = random_number
+    
+    
+    dispatch_all(FUNC_TXT, operat_question, correct_result)
+
+
+
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,12 +4,12 @@
 """Do this module main and import functions from other modules."""
 
 from brain_games.games.do_even import do_even
-from brain_games.central_function import central_function
+
 
 
 def main():
     """Define the main function."""
-    central_function(do_even)
+    do_even()
 
 
 if __name__ == '__main__':
